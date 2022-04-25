e5956b3cc244d24beadf4180c15f9a97c996f499
yuminhwan <ymh7532@icloud.com>
2022-04-25 16:44:45 +0900

    feat: Controller 생성 및 product-list 페이지 구현

--- src/main/java/com/prgrms/gccoffee/controller/ProductController.java
+++ src/main/java/com/prgrms/gccoffee/controller/ProductController.java
@@ -1,4 +1,28 @@
 package com.prgrms.gccoffee.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.prgrms.gccoffee.model.Product;
+import com.prgrms.gccoffee.service.ProductService;
+
+@Controller
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String productsPage(Model model) {
+        List<Product> products = productService.getAllProducts();
+        model.addAttribute("products", products);
+        return "product-list";
+    }
+
 }
